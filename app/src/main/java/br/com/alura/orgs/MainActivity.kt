a4e254fc83479256a61ffc5abcdaa0d6f222bb8d
cabral33 <lucascabral041@gmail.com>
2022-09-18 22:14:24 -0400

    teste

---
+++ app/src/main/java/br/com/alura/orgs/MainActivity.kt
@@ -0,0 +1,19 @@
+package br.com.alura.orgs
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Bem vindo ao Orgs", Toast.LENGTH_LONG).show()
+
+    }
+}
